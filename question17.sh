d18610affa4491e2ce33caacbc6535b94a3c2d8f
Ethan <ethanchu0301@outlook.com>
2024-03-10 14:40:05 +1030

    add

--- question17.sh
+++ question17.sh
@@ -1,8 +1,36 @@
-cd git-practice-02
+cd git-practice-03
 git branch -r
-git checkout -b branch3 origin/branch3
-git checkout -b branch2 origin/branch2
-git merge branch3
-git commit -m "update"
+git checkout -b branch1 
+git checkout -b branch2
 git checkout main
-git branch -D branch3
+
+cd ./dir3
+cp -i bar bar_copy
+git add *
+cd ../
+git commit -m "add"
+
+git checkout branch1
+cd ./dir1/dir2
+git mv foo ../
+cd ../
+git rm -r dir2
+git add *
+git commit -m "update"
+cd ../
+touch newfile1
+git add *
+git commit -m "update"
+
+git checkout branch2
+cd ./dir1/dir2
+git mv foo foo_modified
+git add *
+git commit -m "update"
+cd ../../dir3
+git mv bar newfile2
+git add *
+git commit -m "update"
+cd ../
+git mv dir3 dir1
+git commit -m "finnal update"
